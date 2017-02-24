91e70d4f52816c4f84f75d9a3d76bc687b62204d
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 11:00:56 -0600

    sin retorno con parámetros
    ejemplos de métodos en java con parámetros sin retorno

---
+++ ejemplo2.4.java
@@ -0,0 +1,12 @@
+package metodo2;
+
+public class ejemplo4 {
+	public static void main(String[]args){
+		
+	    suma(30.6,7.3);
+	}
+		public static void suma(double v,double w){
+			double sumar= (v / w);
+			System.out.println("la suma es :"+sumar);
+}
+}
